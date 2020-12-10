03e75d62e1f8aefe305cc41bcacad3d42fbf0e2b
unknown <ebowebdev@gmail.com>
2020-12-10 20:12:27 +0100

    to deploy

--- src/components/CocktailList.js
+++ src/components/CocktailList.js
@@ -4,12 +4,20 @@ import { GlobalContext } from '../Context'
 
 const CocktailList =()=>{
   const { cocktails } = GlobalContext();
-  return (
-    <div className="cocktailList">
-      {console.log(cocktails)}
-      {cocktails.map(cocktail => <Cocktail key={cocktail.id} cocktail={cocktail}/>)}
-    </div>
-  )
+  if(cocktails !== []){
+    return (
+      <div className="cocktailList">
+        {console.log(cocktails)}
+        {cocktails.map(cocktail => <Cocktail key={cocktail.id} cocktail={cocktail}/>)}
+      </div>
+    )
+  }else {
+    return (
+      <div>
+        <h1>wait for the cocktails</h1>
+      </div>
+    )
+  }
 }
 
 export default CocktailList
